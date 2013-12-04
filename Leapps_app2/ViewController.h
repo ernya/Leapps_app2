a41e6aa5f7a879ffd0cf4192a9f72e8bd72f5561
Alexandre ERNY <alexandre.erny@gmail.com>
2013-12-04 16:08:15 -0500

    Base

--- Leapps_app2/ViewController.h
+++ Leapps_app2/ViewController.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MainMenuController.h"
+
+@interface UIImage (scaleToSize)
+- (UIImage*)scaleToSize:(CGSize)size;
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender;
+@end
 
 @interface ViewController : UIViewController
-
+@property (weak, nonatomic) IBOutlet UIView *ApplicationView;
+@property (weak, nonatomic) MainMenuController *_tableView;
 @end
---
+++ MainMenuController.h
@@ -0,0 +1,17 @@
+//
+//  MainMenuController.h
+//  Leapps_app2
+//
+//  Created by Alexandre ERNY on 03/12/2013.
+//  Copyright (c) 2013 Alexandre ERNY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuController : UITableViewController
+
+- (void) settingsPressed;
+
+@property bool _isAdmin;
+@end
+
